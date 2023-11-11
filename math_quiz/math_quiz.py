dd3c945505bd840b5e455f4726549ba6f7b75e39
teodoraspasojevic <spasojevic.teodora@gmail.com>
2023-11-11 16:35:51 +0100

    Worked on code readability, docstrings and exception handling

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,97 @@
 import random
 
 
-def function_A(min, max):
+def generate_random_int(min_value, max_value):
     """
-    Random integer.
+    Generate a random int in range from min to max.
+
+    Args:
+        min_value (float): Minimum value that can be generated.
+        max_value (float): Maximum value that can be generated.
+
+    Returns:
+        int: Randomly generated number in range of min_value and max_value.
     """
-    return random.randint(min, max)
+    # Convert min and max values to ints for function random.randint()
+    min_value = int(min_value)
+    max_value = int(max_value)
+    return random.randint(min_value, max_value)
 
 
-def function_B():
+def generate_random_operator():
+    """
+    Generate a random math operator from list of operations ['+', '-', '*'].
+
+    Returns:
+        str: String representing randomly chosen math operation from list ['+', '-', '*'].
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculate_math_operation(operator_1, operator_2, operation):
+    """
+    Calculate the given math operation over two given operands.
+
+    Args:
+         operator_1(int): First operand participating in math operation.
+         operator_2(int): Second operand participating in math operation.
+         operation(str): Math operation that should be performed on operators.
+    Returns:
+         str: String displaying the performed math operation.
+         int: Result of the calculated math operation.
+    """
+    problem = f"{operator_1} {operation} {operator_2}"
+    if operation == '+':
+        answer = operator_1 + operator_2
+    elif operation == '-':
+        answer = operator_1 - operator_2
+    else:
+        answer = operator_1 * operator_2
+    return problem, answer
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Perform a math quiz.
+
+    The function generates a randomly chosen math operation. Operation is displayed to the user and user's answer is
+    saved. If user's answer is correct, user gets one point.
+
+    User Input:
+        - Integer input for the math quiz answers.
+    Returns:
+        None
+    """
+    score = 0
+    total = 3
 
     print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    print("You will be presented with math problem, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total):
+        # Generate random math operation
+        operator_1 = generate_random_int(1, 10)
+        operator_2 = generate_random_int(1, 5.5)
+        operation = generate_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, answer = calculate_math_operation(operator_1, operator_2, operation)
 
-        if useranswer == ANSWER:
+        # Display the question and save the user's answer
+        print(f"\nQuestion: {problem}")
+        user_answer = input("Your answer: ")
+        try:
+            user_answer = int(user_answer)
+        except ValueError:
+            print("Invalid input. Please enter an int math operation result.")
+
+        # Compare user's answer with correct result of the operation. Catch exception when invalid input.
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
+    print(f"\nGame over! Your score is: {score}/{total}")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
 
 if __name__ == "__main__":
     math_quiz()
